0dca3451b9d138a59d98209e3411eba9e51ef0c0
Mitali92 <60850783+Mitali92@users.noreply.github.com>
2020-03-18 08:58:47 -0400

    Add files via upload

---
+++ add_book.c
@@ -0,0 +1,167 @@
+#include<stdio.h>
+#include <string.h>
+
+#define MAX_BOOK_NAME   50
+#define MAX_AUTHOR_NAME 50
+#define MAX_STUDENT_NAME 50
+#define MAX_STUDENT_ADDRESS 300
+
+
+#define FILE_HEADER_SIZE  sizeof(sFileHeader)
+
+typedef struct
+{
+    int yyyy;
+    int mm;
+    int dd;
+} Date;
+
+
+typedef struct// to call in program
+{
+    unsigned int books_id; // declare the integer data type
+    char bookName[MAX_BOOK_NAME];// declare the character data type
+    char authorName[MAX_AUTHOR_NAME];// declare the charecter data type
+    char studentName[MAX_STUDENT_NAME];// declare the character data type
+    char studentAddr[MAX_STUDENT_ADDRESS];// declare the character data type
+    Date bookIssueDate;// declare the integer data type
+} s_BooksInfo;
+
+
+void printMessageCenter(const char* message)
+{
+    int len =0;
+    int pos = 0;
+    //calculate how many space need to print
+    len = (78 - strlen(message))/2;
+    printf("\t\t\t");
+    for(pos =0 ; pos < len ; pos++)
+    {
+        //print space
+        printf(" ");
+    }
+    //print message
+    printf("%s",message);
+}
+void headMessage(const char *message)
+{
+    system("cls");
+    printf("\t\t\t###########################################################################");
+    printf("\n\t\t\t############                                                   ############");
+    printf("\n\t\t\t############      Library management System Project in C       ############");
+    printf("\n\t\t\t############                                                   ############");
+    printf("\n\t\t\t###########################################################################");
+    printf("\n\t\t\t---------------------------------------------------------------------------\n");
+    printMessageCenter(message);
+    printf("\n\t\t\t----------------------------------------------------------------------------");
+}
+void welcomeMessage()
+{
+    headMessage("www.aticleworld.com");
+    printf("\n\n\n\n\n");
+    printf("\n\t\t\t  **-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**\n");
+    printf("\n\t\t\t        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    printf("\n\t\t\t        =                 WELCOME                   =");
+    printf("\n\t\t\t        =                   TO                      =");
+    printf("\n\t\t\t        =                 LIBRARY                   =");
+    printf("\n\t\t\t        =               MANAGEMENT                  =");
+    printf("\n\t\t\t        =                 SYSTEM                    =");
+    printf("\n\t\t\t        =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    printf("\n\t\t\t  **-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**\n");
+    printf("\n\n\n\t\t\t Enter any key to continue.....");
+    getchar();
+}
+
+int isNameValid(const char *name)
+{
+    int validName = 1;
+    int len = 0;
+    int index = 0;
+    len = strlen(name);
+    for(index =0; index <len ; ++index)
+    {
+        if(!(isalpha(name[index])) && (name[index] != '\n') && (name[index] != ' '))
+        {
+            validName = 0;
+            break;
+        }
+    }
+    return validName;
+}
+
+
+
+void addBookInDataBase()
+{
+    int days;
+    s_BooksInfo addBookInfoInDataBase = {0};
+    FILE *fp = NULL;
+    int status = 0;
+    fp = fopen("D:\\addbook.txt","ab+");
+    if(fp == NULL)
+    {
+        printf("File is not opened\n");
+        exit(1);
+    }
+    headMessage("ADD NEW BOOKS");
+    printf("\n\n\t\t\tENTER YOUR DETAILS BELOW:");
+    printf("\n\t\t\t---------------------------------------------------------------------------\n");
+    printf("\n\t\t\tBook ID NO  = ");
+    fflush(stdin);
+    scanf("%u",&addBookInfoInDataBase.books_id);
+    do
+    {
+        printf("\n\t\t\tBook Name  = ");
+        fflush(stdin);
+        fgets(addBookInfoInDataBase.bookName,MAX_BOOK_NAME,stdin);
+        status = isNameValid(addBookInfoInDataBase.bookName);
+        if (!status)
+        {
+            printf("\n\t\t\tName contain invalid character. Please enter again.");
+        }
+    }
+    while(!status);
+    do
+    {
+        printf("\n\t\t\tAuthor Name  = ");
+        fflush(stdin);
+        fgets(addBookInfoInDataBase.authorName,MAX_AUTHOR_NAME,stdin);
+        status = isNameValid(addBookInfoInDataBase.authorName);
+        if (!status)
+        {
+            printf("\n\t\t\tName contain invalid character. Please enter again.");
+        }
+    }
+    while(!status);
+    do
+    {
+        printf("\n\t\t\tStudent Name  = ");
+        fflush(stdin);
+        fgets(addBookInfoInDataBase.studentName,MAX_STUDENT_NAME,stdin);
+        status = isNameValid(addBookInfoInDataBase.studentName);
+        if (!status)
+        {
+            printf("\n\t\t\tName contain invalid character. Please enter again.");
+        }
+    }
+    while(!status);
+    do
+    {
+        //get date year,month and day from user
+        printf("\n\t\t\tEnter date in format (day/month/year): ");
+        scanf("%d/%d/%d",&addBookInfoInDataBase.bookIssueDate.dd,&addBookInfoInDataBase.bookIssueDate.mm,&addBookInfoInDataBase.bookIssueDate.yyyy);
+        //check date validity
+
+    }
+    while(!status);
+    fwrite(&addBookInfoInDataBase,sizeof(addBookInfoInDataBase), 1, fp);
+    fclose(fp);
+}
+
+
+int main(){
+    welcomeMessage();
+    addBookInDataBase();
+    return 0;
+
+}
